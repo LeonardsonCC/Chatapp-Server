d81907ae4936570d571089ba3e9943e001c467fe
LeonardsonCC <leonardson.carvalho@gmail.com>
2019-11-04 21:45:49 -0300

    added a lot of things

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 let app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
-// let faker = require('faker');
+let CryptoTS = require("crypto-ts");
 
 interface Messages {
     user_id: number
@@ -12,6 +12,7 @@ interface Messages {
 interface User {
     user_id: number
     name: string,
+    session: string
 }
 
 let messages:Array<Messages> = [];
@@ -21,27 +22,30 @@ let message_actual_id:number = 1;
 
 // Socket Events
 io.on('connection', function (socket: SocketIO.Socket) {
-    // Username
+    // New user
     let username = 'Não definido!';
     const user_obj:User = {
         user_id: user_actual_id,
-        name: username
+        name: username,
+        session: CryptoTS.AES.encrypt(username+user_actual_id, "teste").toString(),
     };
     user_actual_id++;
     socket.on('register username', function (username: string) {
         user_obj.name = username;
         console.log(user_obj.name + ' connected');
+        socket.emit('register username success', user_obj.session);
 
-        socket.broadcast.emit('new user connected')
+        socket.broadcast.emit('new user connected');
     });
     
+    // on disconnect
     socket.on('disconnect', function () {
         users = users.filter(item => item.user_id !== user_obj.user_id);
         console.log(username + ' disconnected');
     });    
 });
 
-
+// Run server
 http.listen(3001, function () {
     console.log('server running on 127.0.0.1:3001');
 });
