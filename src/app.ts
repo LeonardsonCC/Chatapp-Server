79ef1650b17465b6c7a9d71e4f6ced56ba67469b
LeonardsonCC <leonardson.carvalho@gmail.com>
2019-11-14 19:29:31 -0300

    prettier and eslint configured

--- src/app.ts
+++ src/app.ts
@@ -19,36 +19,35 @@ let users = new UserList();
 write();
 
 // Socket Events
-io.on(events.CONNECTION, function (socket: SocketIO.Socket) {
-    let actual_user: User;
-    
-    socket.on(events.REGISTER_BY_TOKEN, function (session: string) {
-        actual_user = users.returnLogin(session);
-    });
+io.on(events.CONNECTION, function(socket: SocketIO.Socket) {
+  let actual_user: User;
 
-    socket.on(events.REGISTER_USERNAME, function (username: string) {
-        actual_user = users.userLogin({
-            username
-        });
-        socket.emit(events.REGISTER_USERNAME_RESULT, actual_user.session);
-        socket.broadcast.emit(events.NEW_USER_CONNECTED);
-    });
+  socket.on(events.REGISTER_BY_TOKEN, function(session: string) {
+    actual_user = users.returnLogin(session);
+  });
 
-    socket.on(events.SEND_NEW_MESSAGE, function (message: string) {
-        const new_message = messages.sendNewMessage(actual_user, message);
-        
-        socket.broadcast.emit(events.NEW_MESSAGE, new_message);
-        socket.emit(events.NEW_MESSAGE, new_message);
-
+  socket.on(events.REGISTER_USERNAME, function(username: string) {
+    actual_user = users.userLogin({
+      username,
     });
-    
-    // on disconnect
-    socket.on(events.DISCONNECTED, function () {
-        console.log('user disconnected');
-    });    
+    socket.emit(events.REGISTER_USERNAME_RESULT, actual_user.session);
+    socket.broadcast.emit(events.NEW_USER_CONNECTED);
+  });
+
+  socket.on(events.SEND_NEW_MESSAGE, function(message: string) {
+    const new_message = messages.sendNewMessage(actual_user, message);
+
+    socket.broadcast.emit(events.NEW_MESSAGE, new_message);
+    socket.emit(events.NEW_MESSAGE, new_message);
+  });
+
+  // on disconnect
+  socket.on(events.DISCONNECTED, function() {
+    console.log('user disconnected');
+  });
 });
 
 // Run server
-server.listen(3001, function () {
-    console.log('server running on 127.0.0.1:3001');
+server.listen(3001, function() {
+  console.log('server running on 127.0.0.1:3001');
 });
